f0217ee915f602c010c5b1cccdc25431d25140a9
Aaron Lal <lucidlear@gmail.com>
2023-12-29 19:54:54 +1100

    order of operations

--- ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
+++ ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
@@ -1,12 +1,8 @@
 #include <iostream>
 
 int main() {
-	int x = 10;
-	// int y = x++; // x = 11, y = 10
-	int z = ++x; // x = 11, z = 11
-
+	double x = (1 + 2) * 3;
 	std::cout << x;
-
 	return 0;
 
 }
