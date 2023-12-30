143d7dc3ace8298885f1c896308d759239d4e4ae
Aaron Lal <lucidlear@gmail.com>
2023-12-30 12:56:29 +1100

    sales exercise - my implementation

--- ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
+++ ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
@@ -3,9 +3,17 @@
 using namespace std;
 
 int main() {
-	int x = 10;
-	int y = 20;
-	cout << "x = " << x << endl << "y = " << y;
-	return 0;
 
+	int sales = 95000;
+	const int state_tax_percent = 4;
+	const int county_tax_percent = 2;
+
+	double state_tax = (state_tax_percent / 100.0) * sales;
+	double county_tax = (county_tax_percent / 100.0) * sales;
+	double total_tax = state_tax + county_tax;
+
+	cout << "sales " << sales << endl
+		<< "state_tax " << state_tax << endl
+		<< "county_tax " << county_tax << endl
+		<< "total_tax " << total_tax;
 }
