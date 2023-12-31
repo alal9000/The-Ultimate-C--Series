68bfa32365e963bef2c488de748531179760618b
Aaron Lal <lucidlear@gmail.com>
2023-12-31 12:19:16 +1100

    pointers - updated

--- ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
+++ ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 int main() {
 	int x = 5; // stores the integer 5 in memory location x
-	int* y = &x; // y is a pointer which holds the address of integer x
+	int* y = &x; // y is a pointer which can hold the memory address of an integer, in this case x
 
 	cout << *y;
 	return 0;
