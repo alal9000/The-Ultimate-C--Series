16ca7ed6e14befd307cb5d1d86f064941d4f44d6
Aaron Lal <lucidlear@gmail.com>
2024-04-12 08:32:54 +1000

    latest pointers commit

--- ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
+++ ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
@@ -9,6 +9,10 @@ int main() {
 	int x = 5; // stores the integer 5 in memory location x
 	int* y = &x; // y is a pointer which can hold the memory address of an integer, in this case x
 
-	cout << *y;
+	int* z = static_cast<int*>(malloc(sizeof(int)));
+
+	int* a = z;
+
+	cout << *z;
 	return 0;
 }
