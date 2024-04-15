9d866ce2add9e4f2a2cc4fb9345fffe278babf84
Aaron Lal <lucidlear@gmail.com>
2024-04-15 12:40:42 +1000

    working with booleans

--- ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
+++ ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
@@ -5,14 +5,8 @@
 using namespace std;
 
 int main() {
-	//cout << sizeof(int);
-	double number = numeric_limits<double>::max();
-	//number--;
-
-	cout << number;
-
-	//cout << numeric_limits<int>::min() << endl
-	//	 << numeric_limits<int>::max() << endl;
+	bool isNewUser = false;
+	cout << boolalpha << isNewUser;
 
 
 }
