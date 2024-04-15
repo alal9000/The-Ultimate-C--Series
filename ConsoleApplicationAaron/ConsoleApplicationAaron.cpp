75c8e83cb61d8258361aa94a39cd819bcbc91525
Aaron Lal <lucidlear@gmail.com>
2024-04-15 14:43:05 +1000

    working with arrays

--- ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
+++ ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
@@ -1,56 +1,12 @@
 #include <iostream>
-#include <iomanip>
-#include <string>
 
 
 using namespace std;
 
 int main() {
 
-	// exercise my solution
-
-	//string street;
-	//string city;
-	//string state;
-	//string zip;
-
-	//cout << "Enter your street: ";
-	//getline(cin, street);
-
-	//cout << "Enter your city: ";
-	//getline(cin, city);
-
-	//cout << "Enter your state: ";
-	//getline(cin, state);
-
-	//cout << "Enter your zip: ";
-	//getline(cin, zip);
-
-
-	//cout << street << endl;
-	//cout << city << ", " << state << ", " << zip;
-
-
-	// exercise mosh solution
-
-	string street;
-	cout << "Enter your street: ";
-	getline(cin, street);
-
-	string city;
-	cout << "Enter your city: ";
-	getline(cin, city);
-
-	string state;
-	cout << "Enter your state: ";
-	getline(cin, state);
-	
-	string zip;
-	cout << "Enter your zip: ";
-	getline(cin, zip);
-
-	cout << street << endl
-		 << city << ", " << state << ", " << zip;
+	int numbers[] = {10, 20};
+	cout << numbers[2];
 
 
 	return 0;
