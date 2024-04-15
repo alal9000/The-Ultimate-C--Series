54c56600dc10a68ad92d7af3b213b9636ede0ad0
Aaron Lal <lucidlear@gmail.com>
2024-04-15 12:32:51 +1000

    data types size and limits

--- ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
+++ ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
@@ -5,23 +5,14 @@
 using namespace std;
 
 int main() {
-	cout << left; 
-	cout << setw(10) << "Spring" << setw(10) << "Nice" << endl
-		 << setw(10) << "Summer" << setw(10) << "Hot" << endl;
+	//cout << sizeof(int);
+	double number = numeric_limits<double>::max();
+	//number--;
 
-	cout << fixed << setprecision(10) << 12.34567 << endl
-		 << 22.123456 << endl;
+	cout << number;
 
-	// exercise my solution
-	cout << left;
-	cout << setw(10) << "Course" << setw(10) << "Students" << endl
-	     << setw(10) << "C++" << right << setw(10) << 100 << endl
-		 << setw(10) << "JavaScript" << right << setw(10) << 100 << endl;
-
-	// exercise mosh solution
-	cout << left << setw(15) << "Course" << setw(10) << "Students" << endl
-		<< setw(15) << "C++" << setw(10) << right << 100 << left << endl
-		<< setw(15) << "JavaScript" << setw(10) << right << 50 << left << endl;
+	//cout << numeric_limits<int>::min() << endl
+	//	 << numeric_limits<int>::max() << endl;
 
 
 }
