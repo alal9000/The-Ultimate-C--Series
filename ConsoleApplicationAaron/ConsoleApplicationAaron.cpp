58c34ef9bccf30770b019af1bd3e0140b5dba887
Aaron Lal <lucidlear@gmail.com>
2024-04-15 13:56:48 +1000

    working with chars and strings

--- ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
+++ ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
@@ -1,12 +1,23 @@
 #include <iostream>
 #include <iomanip>
+#include <string>
 
 
 using namespace std;
 
 int main() {
-	bool isNewUser = false;
-	cout << boolalpha << isNewUser;
+	//char ch = 98;
+	//cout << ch;
+
+	string name;
+
+	cout << "Enter your name: ";
+	getline(cin, name);
+	cout << "Hi " << name;
+
+	return 0;
+
+
 
 
 }
