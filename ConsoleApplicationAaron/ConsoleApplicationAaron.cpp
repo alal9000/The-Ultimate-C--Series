5621f768856b0f53f798e90f6a3f80efc3d6a657
Aaron Lal <lucidlear@gmail.com>
2024-04-15 12:18:14 +1000

    formatting output - exercise

--- ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
+++ ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
@@ -10,5 +10,18 @@ int main() {
 		 << setw(10) << "Summer" << setw(10) << "Hot" << endl;
 
 	cout << fixed << setprecision(10) << 12.34567 << endl
-		 << 22.123456;
+		 << 22.123456 << endl;
+
+	// exercise my solution
+	cout << left;
+	cout << setw(10) << "Course" << setw(10) << "Students" << endl
+	     << setw(10) << "C++" << right << setw(10) << 100 << endl
+		 << setw(10) << "JavaScript" << right << setw(10) << 100 << endl;
+
+	// exercise mosh solution
+	cout << left << setw(15) << "Course" << setw(10) << "Students" << endl
+		<< setw(15) << "C++" << setw(10) << right << 100 << left << endl
+		<< setw(15) << "JavaScript" << setw(10) << right << 50 << left << endl;
+
+
 }
