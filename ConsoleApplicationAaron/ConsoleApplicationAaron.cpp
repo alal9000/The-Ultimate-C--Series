3fbe5c11f91fd46f403c267cafb49fd8978513dd
Aaron Lal <lucidlear@gmail.com>
2024-04-16 15:36:30 +1000

    the conditional operator

--- ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
+++ ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
@@ -6,16 +6,31 @@ using namespace std;
 
 int main() {
 
-	bool isCitizen = true;
-	bool caResident = true;
-	short tuition = 0;
+	//int sales = 11'000;
+	//double commission = (sales > 10'000) ? .1 : 0.5;
 
+	//cout << commission;
 
-	if (isCitizen) {
-		if (!caResident) tuition = 1000;
-	}
-	else tuition = 3000;
+	// ex - my implementation
 
+	int number1;
+	int number2;
+	int number3;
+
+	cout << "enter number 1: ";
+	cin >> number1;
+
+	cout << "enter number 2: ";
+	cin >> number2;
+
+	//if (number1 > number2)
+	//	number3 = number1;
+	//else
+	//	number3 = number2;
+
+	number3 = (number1 > number2) ? number1 : number2;
+
+	cout << "The largest value is: " << number3 << endl;
 
 
 	return 0;
