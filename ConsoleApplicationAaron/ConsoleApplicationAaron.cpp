a9f7fface2cc0a59035c1c80f5810adc7d82575a
Aaron Lal <lucidlear@gmail.com>
2024-04-16 18:47:44 +1000

    the for loop

--- ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
+++ ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
@@ -6,29 +6,16 @@ using namespace std;
 
 int main() {
 
-	cout << "1- create account" << endl
-		<< "2- Change password" << endl
-		<< "3- Quit" << endl
-		<< "Select an option: ";
+	for (int i = 5; i > 0; i--) {
+		if (i % 2 != 0 )
+			cout << i << endl;
 
-	short input;
-	cin >> input;
-
-	switch (input) {
-		case 1:
-			cout << "You selected 1";
-			break;
-
-		case 2:
-			cout << "You selected 2";
-			break;
-
-		case 3:
-			cout << "You selected 3";
-			break;
-		default:
-			cout << "Goodbye!";
 	}
+
+	// ex - my solution
+	 
+
+
 
 	
 
