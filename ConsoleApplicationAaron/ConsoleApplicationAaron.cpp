d5d6cca780d7dcac7740706f3cac397ad9285323
Aaron Lal <lucidlear@gmail.com>
2024-04-16 13:49:21 +1000

    order of logical operators

--- ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
+++ ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
@@ -6,10 +6,24 @@ using namespace std;
 
 int main() {
 
-	int age = 20;
-	int salary = 50'000;
-	bool isEligible = (age > 18 && age < 65) || (salary > 30'000);
-	cout << boolalpha << !isEligible;
+	//bool a = true;
+	//bool b = false;
+	//bool c = false;
+	//bool result = (a || b) && c;
+
+	//cout << boolalpha << result;
+
+	// exercise
+
+	bool isCitizen = false;
+	bool hasBachelorsDegree = true;
+	short yearsWorkExperience = 10;
+
+	bool isEligible = isCitizen && (hasBachelorsDegree || yearsWorkExperience >= 2);
+
+	cout << boolalpha << isEligible;
+
+
 
 
 	return 0;
