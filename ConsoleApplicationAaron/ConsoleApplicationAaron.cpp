3e54d6e0c54644f9575e56fd793744fe07ca9ad7
Aaron Lal <lucidlear@gmail.com>
2024-04-17 11:04:11 +1000

    while loops

--- ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
+++ ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
@@ -6,46 +6,23 @@ using namespace std;
 
 int main() {
 
-	//int numbers[] = {1, 2, 3, 4};
-	//for (int i = 0; i < sizeof(numbers) / sizeof(int); i++)
-	//	cout << numbers[i] << endl;
+	/*for (int i = 1; i <= 5; i++)
+		cout << i << endl;
 
-	//for (int number : numbers)
-	//	cout << number << endl;
+	int i = 1;
+	while (i <= 5) {
+		cout << i << endl;
+		i++;
+	}*/
 
-	//string name = "Mosh Hamedani";
+	int number = 0;
+	while (number < 1 || number > 5) {
+		cout << "number: ";
+		cin >> number;
+		if (number < 1 || number > 5)
+			cout << "Enter a number between 1 and 5" << endl;
+	}
 
-	//for (char ch : name)
-	//	cout << ch << endl;
-
-	// ex - my implementation
-
-	//int temps[] = { 20, 38, 40, 50, 9 };
-	//int total = 0;
-	//int count = 0;
-
-	//for (int temp : temps) {
-
-	//	total += temp;
-	//	count++;
-	//}
-
-	//double average = total / static_cast<double>(count);
-
-	//cout << total << endl << count << endl << average;
-
-	// ex - mosh implementation
-
-	int temps[] = { 60, 80, 90 };
-	double sum = 0;
-	short count = sizeof(temps) / sizeof(int);
-
-	for (int temp : temps)
-		sum += temp;
-
-	double average = sum / count;
-
-	cout << average;
 
 
 
