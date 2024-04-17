b8376231d3dd3497bc0f2559d86de1ba567f2ef2
Aaron Lal <lucidlear@gmail.com>
2024-04-17 12:17:07 +1000

    break and continue statements

--- ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
+++ ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
@@ -6,16 +6,22 @@ using namespace std;
 
 int main() {
 
-	int number = 0;
-	do {
+	//for (int i = 1; i <= 5; i++)
+	//{
+	//	if (i % 3 == 0)
+	//		break;
+	//	cout << i << endl;
+
+	//}
+
+	while (true) {
 		cout << "Number: ";
+		int number;
 		cin >> number;
-
-	} while (number < 1 || number > 5);
-
-	
-
-	return 0;
+		if (number >= 1 && number <= 5)
+			break;
+		cout << "Error! Enter a number between 1 and 5" << endl;
+	}
 
 
 
