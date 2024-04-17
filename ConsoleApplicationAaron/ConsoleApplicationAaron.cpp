026eaac39a3744e26936fd9f2e01499a51e0b3c9
Aaron Lal <lucidlear@gmail.com>
2024-04-17 11:15:36 +1000

    do while loops

--- ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
+++ ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
@@ -6,26 +6,14 @@ using namespace std;
 
 int main() {
 
-	/*for (int i = 1; i <= 5; i++)
-		cout << i << endl;
-
-	int i = 1;
-	while (i <= 5) {
-		cout << i << endl;
-		i++;
-	}*/
-
 	int number = 0;
-	while (number < 1 || number > 5) {
-		cout << "number: ";
+	do {
+		cout << "Number: ";
 		cin >> number;
-		if (number < 1 || number > 5)
-			cout << "Enter a number between 1 and 5" << endl;
-	}
 
+	} while (number < 1 || number > 5);
 
-
-
+	
 
 	return 0;
 
