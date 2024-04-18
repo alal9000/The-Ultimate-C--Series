678dc60b940971c2799b50e07b5e8a101daf211d
Aaron Lal <lucidlear@gmail.com>
2024-04-18 16:17:22 +1000

    parameters with a default value

--- ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
+++ ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
@@ -5,25 +5,14 @@
 using namespace std;
 
 
-void greet(string name) {
-	cout << "Hello " << name << endl;
-}
-
-string fullName(string firstName, string lastName) {
-	return firstName + " " + lastName;
+double calculateTax(double income, double expense, double taxRate = .2) {
+	return income * taxRate;
 }
 
 int main() {
 
-	//greet("Mosh", "Hamedani");
-
-
-	//cout << name << endl;
-
-	greet(fullName("Mosh", "Hamedani"));
-
-	cout << "Done";
-
+	double tax = calculateTax(10'000, .3);
+	cout << tax;
 
 
 	return 0;
