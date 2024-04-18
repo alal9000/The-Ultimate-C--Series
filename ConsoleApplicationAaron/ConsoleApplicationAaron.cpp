7e306965ce4018c29bf9a48de47c1686cb12fcae
Aaron Lal <lucidlear@gmail.com>
2024-04-19 09:44:58 +1000

    debugging cpp programs

--- ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
+++ ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
@@ -2,18 +2,19 @@
 #include <string>
 #include "greet.hpp"
 
-
-
 using namespace std;
-using messaging::greet;
+
+void printOddNumbers(int limit) {
+	for (int i = 0; i < limit; i++) {
+		if (i % 2 != 0)
+			cout << i << endl;
+	}
+}
 
 int main() {
-	greet("poop");
-
+	printOddNumbers(10);
 
 	return 0;
-
-
 
 }
 
