d77e355e6ce2c2e09fb5e5664b8b8cc502cf9db8
Aaron Lal <lucidlear@gmail.com>
2024-04-19 10:37:03 +1000

    copying arrays

--- ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
+++ ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
@@ -3,15 +3,18 @@
 using namespace std;
 
 int main() {
-	int numbers[] = {10, 20, 30};
-
-	//for (int number : numbers) {
-	//	cout << number << endl;
-	//}
-
-	for (int i = 0; i < size(numbers); i++) {
-		cout << numbers[i] << endl;
+	int first[] = {10, 20, 30};
+	int second[size(first)];
+	
+	for (int i = 0; i < size(first); i++) {
+		second[i] = first[i];
 	}
+
+	for (int number : second) {
+		cout << number << endl;
+	}
+
+
 	return 0;
 
 }
