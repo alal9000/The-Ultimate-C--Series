91443d13e21b560062fb95fb466c6922753f1f19
Aaron Lal <lucidlear@gmail.com>
2024-04-19 10:18:17 +1000

    creating and initializing arrays

--- ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
+++ ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
@@ -1,18 +1,13 @@
 #include <iostream>
-#include <string>
-#include "greet.hpp"
 
 using namespace std;
 
-void printOddNumbers(int limit) {
-	for (int i = 0; i < limit; i++) {
-		if (i % 2 != 0)
-			cout << i << endl;
-	}
-}
-
 int main() {
-	printOddNumbers(10);
+	int numbers[] = {10, 20};
+	//numbers[0] = 10;
+	//numbers[4] = 20;
+
+	cout << numbers;
 
 	return 0;
 
