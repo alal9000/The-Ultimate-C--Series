08834e0914a9fd0ae8a0ba3b896d196cc8e11651
Aaron Lal <lucidlear@gmail.com>
2024-04-19 10:51:40 +1000

    comparing arrays

--- ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
+++ ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
@@ -3,17 +3,23 @@
 using namespace std;
 
 int main() {
-	int first[] = {10, 20, 30};
-	int second[size(first)];
-	
-	for (int i = 0; i < size(first); i++) {
-		second[i] = first[i];
-	}
+	int first[] = { 10, 20, 30 };
+	int second[] = { 10, 2, 30 };
 
-	for (int number : second) {
-		cout << number << endl;
-	}
+	//if (first == second)
+	//	cout << "Equal";
 
+	//cout << first << endl;
+	//cout << second << endl;
+
+	bool areEqual = true;
+	for (int i =0; i < size(first); i++)
+		if (first[i] != second[i]) {
+			areEqual = false;
+			break;
+		}
+
+	cout << boolalpha << areEqual;
 
 	return 0;
 
