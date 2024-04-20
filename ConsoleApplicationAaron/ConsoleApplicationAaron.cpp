4af7f16a3696c5941836f3031332af67d4b16c35
Aaron Lal <lucidlear@gmail.com>
2024-04-20 13:54:19 +1000

    comparing pointers

--- ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
+++ ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
@@ -2,29 +2,38 @@
 
 using namespace std;
 
-const int rows = 2;
-const int columns = 3;
-
-void printMatrix(int matrix[rows][columns]) {
-	for (int row = 0; row < rows; row++) {
-		for (int column = 0; column < columns; column++) {
-			cout << matrix[row][column] << endl;
-		}
-	}
-
-}
-
 
 int main() {
+	//int x = 10;
+	//int y = 20;
 
-	// 2x3
-	int matrix[rows][columns] = {
-		{11, 12, 13},
-		{21, 22, 23}
-	};
+	//int* ptrX = &x;
+	//int* ptrY = &x;
 
-	printMatrix(matrix);
-	
+	//if (ptrX != nullptr)
+	//	cout << *ptrX;
+
+	// ex - my implementation
+
+	//int numbers[] = { 10, 20, 30 };
+	//int* ptr = &numbers[2];
+
+	//int i = 2;
+	//while (i >= 0) {
+	//	cout << numbers[i] << endl;
+	//	i--;
+
+	//}
+
+	// ex - mosh implementation
+	int numbers[] = { 10, 20, 30 };
+	int* ptr = &numbers[size(numbers) - 1];
+
+	while (ptr >= numbers) {
+		cout << *ptr << endl;
+		ptr--;
+	}
+
 
 
 	return 0;
