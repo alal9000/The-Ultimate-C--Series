8b8991a03dc5e24ddeda7ab6d1fa2f28892d3464
Aaron Lal <lucidlear@gmail.com>
2024-04-20 14:17:47 +1000

    dynamic memory allocation

--- ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
+++ ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
@@ -4,35 +4,18 @@ using namespace std;
 
 
 int main() {
-	//int x = 10;
-	//int y = 20;
+	// stack
+	//int numbers[1000];
+	int x;
 
-	//int* ptrX = &x;
-	//int* ptrY = &x;
+	// heap
+	int* numbers = new int[10];
+	int* number = new int;
+	delete number;
+	delete[] numbers;
 
-	//if (ptrX != nullptr)
-	//	cout << *ptrX;
-
-	// ex - my implementation
-
-	//int numbers[] = { 10, 20, 30 };
-	//int* ptr = &numbers[2];
-
-	//int i = 2;
-	//while (i >= 0) {
-	//	cout << numbers[i] << endl;
-	//	i--;
-
-	//}
-
-	// ex - mosh implementation
-	int numbers[] = { 10, 20, 30 };
-	int* ptr = &numbers[size(numbers) - 1];
-
-	while (ptr >= numbers) {
-		cout << *ptr << endl;
-		ptr--;
-	}
+	number = nullptr;
+	numbers = nullptr;
 
 
 
