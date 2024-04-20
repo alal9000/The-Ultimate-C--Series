fc78a87b79052281c788c8c6adc1da506094e975
Aaron Lal <lucidlear@gmail.com>
2024-04-20 17:50:05 +1000

    extracting substrings

--- ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
+++ ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
@@ -4,13 +4,32 @@ using namespace std;
 
 
 int main() {
-	string name = "Mosh, Hamedani";
-	//cout << name.find('A');
+	//string name = "Mosh Hamedani";
+	//string copy = name.substr(5, 3);
+	//name = "";
 
-	//if (name.find("Ham") == -1)
-	//	cout << "Doesn't exist";
+	//cout << copy;
 
-	cout << name.find_last_not_of(",.;");
+	// ex - my solution
+
+	//string name = "Mosh Hamedani";
+	//string first = name.substr(0, 4);
+	//string last = name.substr(5);
+
+	//cout << first << endl;
+	//cout << last << endl;
+
+	// ex - mosh solution
+
+	string name = "Mosh Smith Hamedani";
+	auto index = name.rfind(' ');
+	string first = name.substr(0, index);
+	string last = name.substr(index + 1);
+
+	cout << first << endl;
+	cout << last << endl;
+
+	
 
 	return 0;
 
