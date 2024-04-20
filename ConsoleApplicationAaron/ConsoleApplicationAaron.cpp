ae9f0c70bcab6655e4a463b64cc55576852be8e6
Aaron Lal <lucidlear@gmail.com>
2024-04-20 12:09:07 +1000

    sorting arrays

--- ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
+++ ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
@@ -2,23 +2,35 @@
 
 using namespace std;
 
+void swap(int numbers[], int i, int j) {
+	int temp = numbers[i];
+	numbers[i] = numbers[j];
+	numbers[j] = temp;
+}
+
+void bubbleSort(int numbers[], int size) {
+
+	for (int pass = 0; pass < size; pass++) {
+		for (int i = 1; i < size; i++) {
+			if (numbers[i] < numbers[i - 1])
+				swap(numbers, i, i - 1);
+
+		}
+	}
+
+
+}
+
 
 int main() {
-	
-	int numbers[] = { 10, 20, 30 };
-	int* ptr = numbers;
-	//ptr++;
-	//ptr--;
-	// 100
+	// bubble sort ex
+	int numbers[] = { 1 };
 
-	cout << *(ptr + 1);
-	cout << ptr[1];
-	cout << numbers[1];
+	bubbleSort(numbers, size(numbers));
 
-
-
-
-
+	for (int number : numbers) {
+		cout << number << endl;
+	}
 
 	return 0;
 
