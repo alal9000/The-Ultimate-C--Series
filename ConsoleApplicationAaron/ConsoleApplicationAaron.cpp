f5dcbaf2a8725c1754d6a48cdc1394258f241e40
Aaron Lal <lucidlear@gmail.com>
2024-04-21 11:54:00 +1000

    dynamically resizing an array

--- ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
+++ ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
@@ -5,10 +5,41 @@ using namespace std;
 
 
 int main() {
+	int capacity = 5;
+	int* numbers = new int[capacity];
+	int entries = 0;
 
-	string str = R"("c:\folderA\folderB\file.txt")";
+	while (true) {
+		cout << "Number: ";
+		cin >> numbers[entries];
+		if (cin.fail()) break;
+		entries++;
+		if (entries == capacity) {
+			// create a temp array (twice the size)
+			// copy all the elements from the old to the new array
+			// have the numbers pointer point to the new array
 
-	cout << str;
+			capacity *= 2;
+			int* temp = new int[capacity];
+
+			for (int i = 0; i < entries; i++)
+			{
+				temp[i] = numbers[i];
+			}
+
+			delete[] numbers;
+			numbers = temp;
+
+		}
+
+	}
+
+	for (int i = 0; i < entries; i++) {
+		cout << numbers[i] << endl;
+
+	}
+
+	delete[] numbers;
 
 	return 0;
 
