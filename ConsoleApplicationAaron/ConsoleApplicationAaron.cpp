09e238d57a6f3bccc4a38490a6798ac99ff91c85
Aaron Lal <lucidlear@gmail.com>
2024-04-21 13:31:48 +1000

    smart pointers

--- ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
+++ ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
@@ -5,41 +5,9 @@ using namespace std;
 
 
 int main() {
-	int capacity = 5;
-	int* numbers = new int[capacity];
-	int entries = 0;
-
-	while (true) {
-		cout << "Number: ";
-		cin >> numbers[entries];
-		if (cin.fail()) break;
-		entries++;
-		if (entries == capacity) {
-			// create a temp array (twice the size)
-			// copy all the elements from the old to the new array
-			// have the numbers pointer point to the new array
-
-			capacity *= 2;
-			int* temp = new int[capacity];
-
-			for (int i = 0; i < entries; i++)
-			{
-				temp[i] = numbers[i];
-			}
-
-			delete[] numbers;
-			numbers = temp;
-
-		}
-
-	}
-
-	for (int i = 0; i < entries; i++) {
-		cout << numbers[i] << endl;
-
-	}
-
-	delete[] numbers;
+	int* x = new int;
+	delete x;
+	
 
 	return 0;
 
