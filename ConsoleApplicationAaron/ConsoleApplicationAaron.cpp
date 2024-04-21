15934bbdd66cd6dc1b648d80a071c1dfba17f3eb
Aaron Lal <lucidlear@gmail.com>
2024-04-21 17:00:57 +1000

    nesting structures

--- ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
+++ ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
@@ -3,24 +3,46 @@
 
 using namespace std;
 
-// PascalCase
+struct Address {
+	int zipcode;
+	string street;
+	string city;
+};
+
+struct Customer {
+	int id;
+	string name;
+	string email;
+	Address address;
+};
+
+struct Date {
+	short year = 1900;
+	short month = 1;
+	short day = 1;
+};
+
 struct Movie {
 	string title;
-	int releaseYear = 0;
+	Date releaseDate;
 	bool isPopular;
 };
 
 
 int main() {
-	vector<Movie> movies;
-	movies.push_back({ "Terminator 1", 1984 });
-	movies.push_back({ "Terminator 2", 1987 });
-	
-	//cout << movies[0].title;
+	//Movie movie{ "Terminator", { 1984, 6, 1 } };
+	//cout << movie.releaseDate.year;
 
-	for (auto& movie : movies)
-		cout << movie.title << endl;
-	
+	// ex
+
+	Customer customer = { 100, "mosh", "mosh@mosh.com", { 200641, "15 bigge street", "Melbourne"} };
+
+	cout << customer.id << endl;
+	cout << customer.name << endl;
+	cout << customer.email << endl;
+	cout << customer.address.city << endl;
+	cout << customer.address.zipcode << endl;
+	cout << customer.address.street << endl;
 
 	return 0;
 
