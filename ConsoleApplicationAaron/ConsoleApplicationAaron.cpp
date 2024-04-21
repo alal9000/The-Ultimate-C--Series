e9ba7f0826ffbaf8f52cfb8c735b893539d54cca
Aaron Lal <lucidlear@gmail.com>
2024-04-21 14:58:33 +1000

    defining structures

--- ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
+++ ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
@@ -3,17 +3,43 @@
 
 using namespace std;
 
+// PascalCase
+struct Movie {
+	string title;
+	int releaseYear;
+};
+
+struct Customer {
+	int ID;
+	string name;
+	string email;
+};
 
 int main() {
-	auto x = make_shared<int>();
-	
-	*x = 10;
-	
-	shared_ptr<int> y(x);
-	if (x == y)
-		cout << "Equal";
 
-	cout << *y;
+	/*Movie movie;
+	movie.title = "Terminator";
+	movie.releaseYear = 1984;
+	cout << "Title: " << movie.title << endl
+		 << "Release Year: " << movie.releaseYear;*/
+
+	// ex
+	Customer customer;
+
+	cout << "Please enter customer ID: " << endl;
+	cin >> customer.ID;
+
+	cout << "Please enter customer name: " << endl;
+	cin >> customer.name;
+
+	cout << "Please enter customer email: " << endl;
+	cin >> customer.email;
+
+	cout << "Customer name: " << customer.name << endl
+		 << "Customer email: " << customer.email << endl
+		 << "Customer ID: " << customer.ID << endl;
+
+	
 
 	return 0;
 
