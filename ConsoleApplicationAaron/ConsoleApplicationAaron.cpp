02f2fdfd65176621b58d49e2263c624eb7426196
Aaron Lal <lucidlear@gmail.com>
2024-04-21 16:17:00 +1000

    initializing structures

--- ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
+++ ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
@@ -6,38 +6,17 @@ using namespace std;
 // PascalCase
 struct Movie {
 	string title;
-	int releaseYear;
+	int releaseYear = 0;
+	bool isPopular;
 };
 
-struct Customer {
-	int ID;
-	string name;
-	string email;
-};
 
 int main() {
 
-	/*Movie movie;
-	movie.title = "Terminator";
-	movie.releaseYear = 1984;
-	cout << "Title: " << movie.title << endl
-		 << "Release Year: " << movie.releaseYear;*/
+	Movie movie = {"Terminator", 1984};
 
-	// ex
-	Customer customer;
-
-	cout << "Please enter customer ID: " << endl;
-	cin >> customer.ID;
-
-	cout << "Please enter customer name: " << endl;
-	cin >> customer.name;
-
-	cout << "Please enter customer email: " << endl;
-	cin >> customer.email;
-
-	cout << "Customer name: " << customer.name << endl
-		 << "Customer email: " << customer.email << endl
-		 << "Customer ID: " << customer.ID << endl;
+	cout << movie.releaseYear;
+	
 
 	
 
