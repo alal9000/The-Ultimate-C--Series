87bd050ff7e57a4157f26ca86244942b579cd886
Aaron Lal <lucidlear@gmail.com>
2024-04-22 17:38:51 +1000

    structures and functions

--- ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
+++ ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
@@ -1,66 +1,52 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-// ex my solution
-//bool isValid(string customerNumber) {
-//	bool valid = true;
-//
-//	if (customerNumber.length() != 6)
-//		return valid = false;
-//
-//
-//	for (int i = 0; i < 2; i++) {
-//		if (!isalpha(customerNumber[i]))
-//			return valid = false;
-//	}
-//
-//	for (int i = 2; i < 6; i++) {
-//		if (!isdigit(customerNumber[i]))
-//			return valid = false;
-//	}
-//
-//	return valid;
-//
-//}
+struct Date {
+	short year = 1900;
+	short month = 1;
+	short day = 1;
+};
 
-// ex mosh solution
-bool isValid(string customerNumber) {
+struct Movie {
+	string title;
+	Date releaseDate;
+	bool isPopular;
 
-	if (customerNumber.length() != 6)
-		return false;
+	// Methods
 
-	for (int i = 0; i < 2; i++) {
-		if (!isalpha(customerNumber[i]))
-			return false;
-	}
+};
 
-	for (int i = 2; i < customerNumber.length(); i++) {
-		if (!isdigit(customerNumber[i]))
-			return false;
-	}
-
-	return true;
+bool operator==(const Movie& first, const Movie& second) {
+	return (
+		first.title == second.title &&
+		first.releaseDate.year == second.releaseDate.year &&
+		first.releaseDate.month == second.releaseDate.month &&
+		first.releaseDate.day == second.releaseDate.day
+		);
 
 }
 
+ostream& operator<<(ostream& stream, const Movie& movie) {
+	stream << movie.title;
+	return stream;
+}
+
+Movie getMovie() {
+	return { "Terminator", 1984 };
+}
+
+void showMovie(Movie& movie) {
+	cout << movie.title;
+}
+
+
+
 
 int main() {
-	//string name = " Mosh Hamedani";
-	//cout << (char) toupper('a');
-
-	//char input;
-	//if (tolower(input) == 'x')
-	//	break;
-
-	// ex
-
-	//bool result = isValid("AB1234");
-
-	//cout << boolalpha << result;
-
-	string customerNumber = "1B1234";
-	cout << isValid(customerNumber);
+	auto movie = getMovie();
+	showMovie(movie);
 
 
 	return 0;
