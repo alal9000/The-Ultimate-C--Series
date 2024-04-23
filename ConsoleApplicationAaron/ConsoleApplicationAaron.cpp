6d702204428b36fb041e2e4aecba6823c1bd9acc
Aaron Lal <lucidlear@gmail.com>
2024-04-24 09:39:44 +1000

    writing to text files

--- ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
+++ ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
@@ -1,34 +1,22 @@
 #include <iostream>
+#include <fstream>
+#include <iomanip>
 
 using namespace std;
 
 
-// ex - mine and mosh solution
-int readValue(const string& message) {
-	int number;
-	while (true) {
-		cout << message << ": ";
-		cin >> number;
-		if (cin.fail()) {
-			cout << "Enter a valid number" << endl;
-			cin.clear();
-			cin.ignore(numeric_limits<streamsize>::max(), '\n');
-		}
-		else break;
-		
-
-	}
-	return number;
-
-}
-
-
 int main() {
-	
-	int first = readValue("first");
-	int second = readValue("Second");
+	ofstream file;
+	file.open("data.csv");
+	if (file.is_open()) {
+		file << "id,title,year\n"
+			 << "1,Terminator 1,1984\n"
+			 << "2,Terminator 2,1991\n";
+		file.close();
+	}
 
-	cout << "You entered " << first << " and " << second;
+	
+
 
 
 	return 0;
