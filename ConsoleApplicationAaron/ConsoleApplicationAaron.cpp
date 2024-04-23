65c38e0d7a0bac2bebd18b1380ee8b591fdfa244
Aaron Lal <lucidlear@gmail.com>
2024-04-24 08:07:40 +1000

    reading from streams

--- ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
+++ ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
@@ -2,37 +2,19 @@
 
 using namespace std;
 
-enum class Action {
-	List = 1,
-	Add = 2,
-	Update = 3
-};
-
-enum class Operation {
-	List = 1,
-	Add = 2,
-	Update = 3
-};
-
 
 int main() {
-	//const int list = 1;
-	//const int add = 2;
-	//const int update = 3;
-	cout <<
-		"1: List invoices" << endl <<
-		"2: Add invoice" << endl <<
-		"3: Update invoices" << endl <<
-		"Select: ";
+	int first;
+	int second;
 
-	int input;
-	cin >> input;
+	cout << "First: ";
+	cin >> first;
+	cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
-	if (input == static_cast<int>(Action::List)) {
-		cout << "List invoices";
+	cout << "Second: ";
+	cin >> second;
 
-	}
-
+	cout << "You entered " << first << " and " << second;
 
 
 	return 0;
