fa66945bab368b0e6c026780a49a319a30390908
Aaron Lal <lucidlear@gmail.com>
2024-04-24 08:35:59 +1000

    handling input errors

--- ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
+++ ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
@@ -3,16 +3,30 @@
 using namespace std;
 
 
+// ex - mine and mosh solution
+int readValue(const string& message) {
+	int number;
+	while (true) {
+		cout << message << ": ";
+		cin >> number;
+		if (cin.fail()) {
+			cout << "Enter a valid number" << endl;
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		}
+		else break;
+		
+
+	}
+	return number;
+
+}
+
+
 int main() {
-	int first;
-	int second;
-
-	cout << "First: ";
-	cin >> first;
-	cin.ignore(numeric_limits<streamsize>::max(), '\n');
-
-	cout << "Second: ";
-	cin >> second;
+	
+	int first = readValue("first");
+	int second = readValue("Second");
 
 	cout << "You entered " << first << " and " << second;
 
