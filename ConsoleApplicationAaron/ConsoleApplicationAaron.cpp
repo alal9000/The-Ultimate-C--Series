3634a52dbc0719a364db933b0717e464e036b147
Aaron Lal <lucidlear@gmail.com>
2024-04-24 10:52:12 +1000

    reading from text files

--- ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
+++ ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
@@ -1,22 +1,41 @@
 #include <iostream>
 #include <fstream>
-#include <iomanip>
+#include <string>
 
 using namespace std;
 
+struct Movie {
+	int id;
+	string title;
+	int year;
+};
+
 
 int main() {
-	ofstream file;
+	ifstream file;
 	file.open("data.csv");
 	if (file.is_open()) {
-		file << "id,title,year\n"
-			 << "1,Terminator 1,1984\n"
-			 << "2,Terminator 2,1991\n";
+		string str;
+		getline(file, str);
+		while (!file.eof()) {
+		getline(file, str, ',');
+		if (str.empty()) continue;
+
+
+		Movie movie;
+		movie.id = stoi(str);
+
+		getline(file, str, ',');
+		movie.title = str;
+
+		getline(file, str);
+		movie.year = stoi(str);
+
+		cout << movie.title << endl;
+
+		}
 		file.close();
 	}
-
-	
-
 
 
 	return 0;
