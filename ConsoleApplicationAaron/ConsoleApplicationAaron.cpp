5a2394be1cbba60f6aee23c8b07664fb2d8b26af
Aaron Lal <lucidlear@gmail.com>
2024-04-24 16:39:42 +1000

    reading from binary files

--- ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
+++ ConsoleApplicationAaron/ConsoleApplicationAaron.cpp
@@ -12,11 +12,13 @@ struct Movie {
 
 
 int main() {
-	int numbers[] = { 1'000'000, 2'000'000, 3'000'000 };
+	int numbers[3];
 
-	ofstream file("numbers2.dat", ios::binary);
+	ifstream file("numbers.dat", ios::binary);
 	if (file.is_open()) {
-		file.write(reinterpret_cast<char*>(&numbers), sizeof(numbers));
+		int number;
+		while (file.read(reinterpret_cast<char*>(&number), sizeof(number))) 
+			cout << number;
 		file.close();
 	}
 
